5d305a9b4448eb196322201bb2393f89c9f45caa
Italo Duarte <italod21@gmail.com>
2025-11-11 16:47:06 -0300

    Criação da página de login

---
+++ src/scripts/start.js
@@ -0,0 +1,6 @@
+//const btn = document.getElementById('start');
+const button = document.querySelector('#start');
+
+button.addEventListener('click', () => {
+ window.location.href = './src/pages/login.html';
+});
